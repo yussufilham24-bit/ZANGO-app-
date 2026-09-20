0d3d05dd223cf35214145c485c5fc1ad62503ee0
yussufilham24-bit <yussufilham24@gmail.com>
2026-09-20 09:42:50 +0300

    Create Service worker.js

---
+++ Service worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "zango-app-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
